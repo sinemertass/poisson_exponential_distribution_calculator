d8da96084b1053bfca1fed0131f82f22468da16f
sinemertass <49760031+sinemertass@users.noreply.github.com>
2020-03-27 17:50:06 +0300

    Add files via upload

---
+++ WindowsFormsApp11/WindowsFormsApp11/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace WindowsFormsApp11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.txta1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGoster = new System.Windows.Forms.Button();
+            this.txta2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbPoisson = new System.Windows.Forms.RadioButton();
+            this.rbUstel = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSonuc1 = new System.Windows.Forms.TextBox();
+            this.txtSonuc2 = new System.Windows.Forms.TextBox();
+            this.txtSonuc3 = new System.Windows.Forms.TextBox();
+            this.txtSonuc4 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.txtLambda = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txta1
+            // 
+            this.txta1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txta1.Location = new System.Drawing.Point(158, 29);
+            this.txta1.Name = "txta1";
+            this.txta1.Size = new System.Drawing.Size(107, 34);
+            this.txta1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "● a₁  Sayısı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(271, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "● a₂ Sayısı";
+            // 
+            // btnGoster
+            // 
+            this.btnGoster.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGoster.Location = new System.Drawing.Point(515, 32);
+            this.btnGoster.Name = "btnGoster";
+            this.btnGoster.Size = new System.Drawing.Size(210, 133);
+            this.btnGoster.TabIndex = 4;
+            this.btnGoster.Text = "    Olasılık Değerlerini     ve Grafiği Göster";
+            this.btnGoster.UseVisualStyleBackColor = true;
+            this.btnGoster.Click += new System.EventHandler(this.btnGoster_Click);
+            // 
+            // txta2
+            // 
+            this.txta2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txta2.Location = new System.Drawing.Point(402, 29);
+            this.txta2.Name = "txta2";
+            this.txta2.Size = new System.Drawing.Size(107, 34);
+            this.txta2.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtLambda);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.rbUstel);
+            this.groupBox1.Controls.Add(this.btnGoster);
+            this.groupBox1.Controls.Add(this.rbPoisson);
+            this.groupBox1.Controls.Add(this.txta1);
+            this.groupBox1.Controls.Add(this.txta2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(25, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(750, 186);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // rbPoisson
+            // 
+            this.rbPoisson.AutoSize = true;
+            this.rbPoisson.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbPoisson.Location = new System.Drawing.Point(24, 132);
+            this.rbPoisson.Name = "rbPoisson";
+            this.rbPoisson.Size = new System.Drawing.Size(215, 33);
+            this.rbPoisson.TabIndex = 7;
+            this.rbPoisson.TabStop = true;
+            this.rbPoisson.Text = "Poisson Dağılımı";
+            this.rbPoisson.UseVisualStyleBackColor = true;
+            // 
+            // rbUstel
+            // 
+            this.rbUstel.AutoSize = true;
+            this.rbUstel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbUstel.Location = new System.Drawing.Point(276, 132);
+            this.rbUstel.Name = "rbUstel";
+            this.rbUstel.Size = new System.Drawing.Size(177, 33);
+            this.rbUstel.TabIndex = 8;
+            this.rbUstel.TabStop = true;
+            this.rbUstel.Text = "Üstel Dağılım";
+            this.rbUstel.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtSonuc4);
+            this.groupBox2.Controls.Add(this.txtSonuc3);
+            this.groupBox2.Controls.Add(this.txtSonuc2);
+            this.groupBox2.Controls.Add(this.txtSonuc1);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(25, 203);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(320, 254);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(19, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 29);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "P(a₁<x<a₂)=";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(19, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 29);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "P(x≤a₂)=";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(19, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 29);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "P(a₁≤x)=";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(19, 184);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(169, 29);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "P(x<a₁ | x<a₂)=";
+            // 
+            // txtSonuc1
+            // 
+            this.txtSonuc1.Location = new System.Drawing.Point(184, 29);
+            this.txtSonuc1.Multiline = true;
+            this.txtSonuc1.Name = "txtSonuc1";
+            this.txtSonuc1.Size = new System.Drawing.Size(107, 29);
+            this.txtSonuc1.TabIndex = 9;
+            // 
+            // txtSonuc2
+            // 
+            this.txtSonuc2.Location = new System.Drawing.Point(184, 80);
+            this.txtSonuc2.Multiline = true;
+            this.txtSonuc2.Name = "txtSonuc2";
+            this.txtSonuc2.Size = new System.Drawing.Size(107, 29);
+            this.txtSonuc2.TabIndex = 10;
+            // 
+            // txtSonuc3
+            // 
+            this.txtSonuc3.Location = new System.Drawing.Point(184, 131);
+            this.txtSonuc3.Multiline = true;
+            this.txtSonuc3.Name = "txtSonuc3";
+            this.txtSonuc3.Size = new System.Drawing.Size(107, 29);
+            this.txtSonuc3.TabIndex = 11;
+            // 
+            // txtSonuc4
+            // 
+            this.txtSonuc4.Location = new System.Drawing.Point(184, 184);
+            this.txtSonuc4.Multiline = true;
+            this.txtSonuc4.Name = "txtSonuc4";
+            this.txtSonuc4.Size = new System.Drawing.Size(107, 29);
+            this.txtSonuc4.TabIndex = 12;
+            // 
+            // chart1
+            // 
+            chartArea3.BackImageAlignment = System.Windows.Forms.DataVisualization.Charting.ChartImageAlignmentStyle.Center;
+            chartArea3.BackImageWrapMode = System.Windows.Forms.DataVisualization.Charting.ChartImageWrapMode.TileFlipXY;
+            chartArea3.CursorX.Interval = 0D;
+            chartArea3.CursorX.IntervalOffset = 40D;
+            chartArea3.CursorX.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea3.CursorX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea3.CursorX.LineWidth = 2;
+            chartArea3.CursorY.Interval = 0D;
+            chartArea3.CursorY.IntervalOffset = 4D;
+            chartArea3.CursorY.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea3.CursorY.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea3.Name = "Chat area";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            legend3.IsTextAutoFit = false;
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(351, 218);
+            this.chart1.Name = "chart1";
+            series7.ChartArea = "Chat area";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            series7.Legend = "Legend1";
+            series7.Name = "λ=0.5";
+            series8.ChartArea = "Chat area";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            series8.Legend = "Legend1";
+            series8.Name = "λ=1";
+            series9.ChartArea = "Chat area";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            series9.Legend = "Legend1";
+            series9.Name = "λ=1.5";
+            this.chart1.Series.Add(series7);
+            this.chart1.Series.Add(series8);
+            this.chart1.Series.Add(series9);
+            this.chart1.Size = new System.Drawing.Size(424, 239);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            title3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            title3.Name = "Title1";
+            title3.Text = "P(x) grafiği";
+            this.chart1.Titles.Add(title3);
+            // 
+            // txtLambda
+            // 
+            this.txtLambda.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtLambda.Location = new System.Drawing.Point(402, 67);
+            this.txtLambda.Name = "txtLambda";
+            this.txtLambda.Size = new System.Drawing.Size(107, 34);
+            this.txtLambda.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(271, 67);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(133, 29);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "●  λ Değeri";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 496);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txta1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGoster;
+        private System.Windows.Forms.TextBox txta2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbUstel;
+        private System.Windows.Forms.RadioButton rbPoisson;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtSonuc4;
+        private System.Windows.Forms.TextBox txtSonuc3;
+        private System.Windows.Forms.TextBox txtSonuc2;
+        private System.Windows.Forms.TextBox txtSonuc1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox txtLambda;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ WindowsFormsApp11/WindowsFormsApp11/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        private double a1;
+        private double a2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGoster_Click(object sender, EventArgs e)
+        {
+            txtSonuc1.Clear();
+            txtSonuc2.Clear();
+            txtSonuc3.Clear();
+            txtSonuc4.Clear();
+
+            if (txta1.Text != "")
+             {
+                double a1 = Convert.ToDouble(txta1.Text);
+             }
+                if (txta2.Text != "")
+             {
+                double a2 = Convert.ToDouble(txta2.Text);
+             }
+                if ((String.IsNullOrEmpty(txta1.Text)) && (String.IsNullOrEmpty(txta2.Text)))
+             {
+                 MessageBox.Show("BİR SAYI GİRİNİZ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+             }
+
+            if (rbPoisson.Checked)
+            {
+                if (String.IsNullOrEmpty(txtLambda.Text))
+                {
+                    MessageBox.Show("LAMBDA DEĞERİ GİRİN", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                { 
+                double lambda = Convert.ToDouble(txtLambda.Text);
+                double numerator = 0;
+                double p;
+
+                double Factorial(double k)
+                {
+                    double count = k;
+                    double factorial = 1;
+                    while (count >= 1)
+                    {
+                        factorial = factorial * count;
+                        count--;
+                    }
+                    return factorial;
+                }
+
+                p = 0;
+                for (double k = a1; k <= a2; k++)
+                {
+                    double kFactorial = Factorial(k);
+                    numerator = Math.Pow(Math.E, -(double)lambda) * Math.Pow((double)lambda, (double)k);
+                    p = p + (double)numerator / kFactorial;
+                }
+                txtSonuc1.Text = p.ToString();
+
+                p = 0;
+
+                // X<=a2; yani a2 ve a2 den küçüklerin toplamı. (0 a kadar)
+                for (double k = 0; k <= a2; k++)
+                {
+                    double kFactorial = Factorial(k);
+                    numerator = Math.Pow(Math.E, -(double)lambda) * Math.Pow((double)lambda, (double)k);
+                    p = p + (double)numerator / kFactorial;
+                }
+                txtSonuc2.Text = p.ToString();
+                p = 0;
+
+                // a1<=X yani 1-a1 den küçük olanlar
+                for (double k = 0.0; k < a1; k++)
+                {
+
+                    double kFactorial = Factorial(k);
+                    numerator = Math.Pow(Math.E, -(double)lambda) * Math.Pow((double)lambda, (double)k);
+                    p = p + (double)numerator / kFactorial;
+                    p = 1 - p;
+                }
+                txtSonuc3.Text = p.ToString();
+                    
+                }
+            }
+        }
+    }
+}
